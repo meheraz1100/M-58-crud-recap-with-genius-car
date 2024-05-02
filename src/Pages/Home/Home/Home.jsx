d9e58357f68337880f7442515c8d492bf4c5ea7e
meheraz1100 <meheraz1100@gmail.com>
2024-05-02 20:11:55 +0600

    project setup with resources

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <h1 className="text-3xl">this is home</h1>            
+        </div>
+    );
+};
+
+export default Home;
